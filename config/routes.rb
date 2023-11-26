fe865e11a998ac9b239ad30dcc5f9f226235490f
oishi <xospan@outlook.jp>
2023-11-26 22:10:42 +0900

    User model creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "sessions#new"
-  get "/signin", to:"users#new"
-  get "/profile-edit", to:"users#profile-edit"
+  get "/signup", to:"users#new"
+  get "/introduce-edit", to:"users#edit"
   # get "/top-loggedin", to:"users#loggedin"
   # get "/skill-edit", to:"skills#edit"
   # get "/skill-new", to:"skills#skill-new"
