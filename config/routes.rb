daf06f84449df1f853fb941f36f5694979e1a71f
oishi <xospan@outlook.jp>
2024-02-18 02:04:05 +0900

    Pulldown malfunction - resolved

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,6 @@ Rails.application.routes.draw do
   post   "/login",   to: "sessions#create"
   get    "/logout_path",  to: "sessions#destroy"
 
-  get    "users/index",      to: "users#index"
-  post   "users/create",     to: "users#create"
-  get    "users/new",        to: "users#new"
-  get    "users/:id/edit",   to: "users#edit"
-  get    "users/:id",        to: "users#show"
-  patch  "users/:id",        to: "users#update"
-  put    "users/:id",        to: "users#update"
-  delete "users/:id",        to: "users#destroy"
-
   get   "skills/skill_create_back_this_month",  to: "skills#skill_create_back_this_month"
   get   "skills/skill_create_back_last_month",  to: "skills#skill_create_back_last_month"
   get   "skills/skill_create_back_two_month",  to: "skills#skill_create_back_two_month"
@@ -24,10 +15,8 @@ Rails.application.routes.draw do
   get   "skills/skill_create_infra_last_month",  to: "skills#skill_create_infra_last_month"
   get   "skills/skill_create_infra_two_month",  to: "skills#skill_create_infra_two_month"
 
-
   resources :users
   resources :skills
-  resources :learnings
   
   get 'users/index'
 
