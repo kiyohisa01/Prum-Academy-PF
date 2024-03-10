c511fac925373d0908e23b8c576486a776a5d8ff
oishi <xospan@outlook.jp>
2024-03-10 14:21:29 +0900

    turbo:install stimulus:install

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -7,15 +7,3 @@ application.debug = false
 window.Stimulus   = application
 
 export { application }
-
-
-// let this_month_back_sum = gon.this_month_back_sum
-// $('#this_month_back_sum').html(this_month_back_sum);
-
-//= require jquery
-//= require jquery_ujs
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-
-
